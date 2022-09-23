712d3d87e279e4319d0b3a926b026b3cb8bf66e7
mostafa82020mohamedy <44113074+mostafa82020mohamedy@users.noreply.github.com>
2022-09-23 11:42:29 +0200

    showing job list and job details

--- job/views.py
+++ job/views.py
@@ -1,3 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import job
 
 # Create your views here.
+def job_list(request):
+    job_list=job.objects.all()
+    context={'jobs':job_list}
+    return render(request, 'job\job_list.html ' , context)
+
+
+def job_detail(request , id):
+    job_detail=job.objects.get(id=id)
+    context={'job':job_detail}
+    return render(request, 'job\job_detail.html ' , context)
+
+
+
